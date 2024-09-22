b80b086416abaaf9a386fd8524b1620ebdccf85f
Darko Lukic <lukicdarkoo@gmail.com>
2024-09-22 23:40:53 +0200

    add license

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='teleop',
-    version='0.1',
+    version='0.0.1',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'Flask',
         'numpy',
